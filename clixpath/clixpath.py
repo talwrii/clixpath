3cc40309b986e5a34cdf44f51183a36a82f123a7
Tal Wrii <talwrii@gmail.com>
2016-12-29 22:39:01 +0000

    Include path to element in json output

--- clixpath/clixpath.py
+++ clixpath/clixpath.py
@@ -20,6 +20,32 @@ def main():
         print(result)
         sys.stdout.flush()
 
+def get_element_path(elt):
+    items = []
+    while elt is not None:
+        try:
+            tag = elt.tag
+        except AttributeError:
+            elt = elt.getparent()
+            continue
+
+        try:
+            attribs = elt.attrib
+            shown_attribs = {k:v for (k, v) in attribs.items() if k in ('id', 'class')}
+            attrib_text = ' and '.join('@{}="{}"'.format(k, v) for k, v in shown_attribs.items())
+        except AttributeError:
+            attrib_text = None
+
+        if attrib_text:
+            items.append('{}[{}]'.format(tag, attrib_text))
+        else:
+            items.append(tag)
+
+        elt = elt.getparent()
+        if elt is None:
+            break
+    return '/' + '/'.join(reversed(items))
+
 def run(args, input_stream):
     options = build_parser().parse_args(args)
     del args
@@ -27,14 +53,15 @@ def run(args, input_stream):
     result = []
     xml_entries = []
     for elt in tree.xpath(options.xpath):
+        path = get_element_path(elt)
         if isinstance(elt, str):
             markup = str(elt)
         else:
             markup = lxml.etree.tostring(elt)
 
         result.append(markup)
-        xml_entries.append(dict(markup=markup))
+        xml_entries.append(dict(markup=markup, path=path))
     if options.json:
-        return json.dumps(xml_entries)
+        return json.dumps(xml_entries, indent=4)
     else:
         return '\n'.join(result)
