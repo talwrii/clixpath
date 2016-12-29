76441894ad8f193184343fba72668cb90ea81694
Tal Wrii <talwrii@gmail.com>
2016-12-29 22:49:18 +0000

    Ensure that the readme has not changed

--- make-readme.py
+++ make-readme.py
@@ -20,13 +20,11 @@ def make_readme_text():
 
 def backticks(command, stdin=None, shell=False):
     stdin_arg = subprocess.PIPE if stdin is not None else None
-
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stdin=stdin_arg, shell=shell)
     result, _ = process.communicate(stdin)
     if process.returncode != 0:
         raise Exception('{!r} returned non-zero return code {!r}'.format(command, process.returncode))
     return result
-
 
 def main():
     PARSER = argparse.ArgumentParser(description='Write readme.md')
--- tests/test_clixpath.py
+++ tests/test_clixpath.py
@@ -1,9 +1,13 @@
 # make code as python 3 compatible as possible
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
 
 import json
+import os
 import shutil
 import StringIO
+import subprocess
+import sys
 import tempfile
 import unittest
 
@@ -21,13 +25,19 @@ class TestClix(unittest.TestCase):
         input_string = args[-1]
         return run(args[:-1], StringIO.StringIO(input_string))
 
+    def test_readme(self):
+        HERE = os.path.dirname(__file__) or '.'
+        readme = backticks([sys.executable, os.path.join(HERE, '..', 'make-readme.py'), '--stdout'])
+        with open(os.path.join(HERE, '..', 'README.md')) as stream:
+            readme_file_text = stream.read()
+        self.assertEquals(readme_file_text.strip('\n'), readme.strip('\n'))
+
     def test_basic(self):
         TEXT = '''
         <html>
         <body><a href="blah">hello</a></body>
         </html>
         '''
-
         entry, = json.loads(self.run_cli('//a', '--json', TEXT))
         self.assertEquals(entry['markup'], '<a href="blah">hello</a>')
 
@@ -35,6 +45,16 @@ class TestClix(unittest.TestCase):
         self.assertEquals(entry['markup'], 'blah')
 
         self.run_cli('//a', TEXT)
+
+def backticks(command, stdin=None, shell=False):
+    stdin_arg = subprocess.PIPE if stdin is not None else None
+
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stdin=stdin_arg, shell=shell)
+    result, _ = process.communicate(stdin)
+    if process.returncode != 0:
+        raise Exception('{!r} returned non-zero return code {!r}'.format(command, process.returncode))
+    return result
+
 
 if __name__ == '__main__':
     unittest.main()
