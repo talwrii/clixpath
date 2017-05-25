6149137c9bb685a10bf80437de3ebc9822620ef4
Tal Wrii <talwrii@gmail.com>
2017-05-25 17:33:02 +0100

    clixpath -- various operations for filtering

--- clixpath/clixpath.py
+++ clixpath/clixpath.py
@@ -17,6 +17,9 @@ def build_parser():
     parser.add_argument('--json', '-J', action='store_true', help='Produce output in machine readable json')
     parser.add_argument('xpath', type=str, help='Xpath expression')
     parser.add_argument('file', type=str, nargs='*', help='File to operate on. (Path included in json)')
+    parser.add_argument('--drop', '-d', type=str, action='append', help='Delete xpaths from result')
+    parser.add_argument('--extract', '-x', type=str, action='append', nargs=2, help='Takes args KEY XPATH and extact XPATH from matches and stores in in KEY')
+    parser.add_argument('--no-key', '-n', type=str, action='append', help='Remove this key from json output')
     return parser
 
 def main():
@@ -62,9 +65,27 @@ def run(args, input_stream):
         streams = [(os.path.abspath(f), open(f)) for f in options.file]
 
     for path, stream in streams:
-        for entry in parse_stream(options.xpath, stream):
+        for entry in parse_stream(options.xpath, stream, options.drop or []):
             entry['file'] = path
+
+            key_values = dict()
+            if options.extract:
+                for key, xpath in options.extract:
+                    values = entry['tree'].xpath(xpath)
+                    if len(values) == 1:
+                        values, = values
+                    elif len(values) == 0:
+                        values = None
+                    key_values[key] = values
+
             if options.json:
+                entry = dict(entry, **key_values)
+                del entry['tree']
+
+                if options.no_key:
+                    for key in options.no_key:
+                        del entry[key]
+
                 yield json.dumps(entry, sort_keys=True) + "\n"
             else:
                 yield entry['markup'] + "\n"
@@ -75,6 +96,14 @@ def parse_stream(xpath, input_stream):
     result = []
     xml_entries = []
     for elt in tree.xpath(xpath):
+        for drop_path in drop:
+            while True:
+                # avoid deleting something we have already deleted
+                to_drop = elt.xpath(drop_path)
+                if not to_drop:
+                    break
+                to_drop[0].getparent().remove(to_drop[0])
+
         path = get_element_path(elt)
         if isinstance(elt, (str, unicode)):
             markup = unicode(elt)
